f0890a3b9eeb289399c3636987e74ef3f2c164cc
yue <leticiabholanda@gmail.com>
2026-08-06 16:32:05 -0300

    wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,55 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 float getTemperature();
 float getHumidity();
 float getLuminosity();
 
+// variáveis para conexão WiFi (nome da rede e senha) -> pegar wifi e senha a partir da aplicacao web depois? por seguranca
+String ssid;
+String pswd;
+
 void setup()
 {
+  // inicializando a comunicação serial
+  Serial.begin(115200);
+
+  // recebendo o nome da rede e a senha do wifi através do monitor serial (temporario?)
+  Serial.println("Insira o nome da rede Wifi: ");
+  if (Serial.available())
+  {
+    ssid = Serial.readStringUntil('\n');
+  }
+  Serial.println("Insira a senha da rede Wifi: ");
+  if (Serial.available())
+  {
+    pswd = Serial.readStringUntil('\n');
+  }
+
+  // inicializando a comunicação wifi
+  WiFi.begin(ssid, pswd);
+  // aguardando a conexão para continuar o programa
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(300);
+    Serial.println("Conectando com a rede WiFi...");
+  }
+  Serial.println("Conectado à rede Wifi.");
 }
 
 void loop()
 {
+  // verificando se a conexão foi perdida
+  if (WiFi.status() == WL_CONNECTION_LOST)
+  {
+    // tentando reconectar
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, pswd);
+      Serial.println("Conexão com a rede WiFi perdida. Tentando reconectar...");
+    }
+    Serial.println("Reconectado à rede Wifi.");
+  }
 }
 
 // função para gerar um valor de temperatura
