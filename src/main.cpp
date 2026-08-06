d9a155ff1821e006c62b49e2e0cea94778b40dfe
yue <leticiabholanda@gmail.com>
2026-08-06 15:25:14 -0300

    sensor value simulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+float getTemperature();
+float getHumidity();
+float getLuminosity();
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// função para gerar um valor de temperatura
+float getTemperature()
+{
+  // gerando um valor aleatório entre 160 (inclusivo) e 331 (exclusivo) e dividindo por 10 para
+  // transformar o inteiro em um float indicando uma temperatura com até uma casa decimal de precisão
+  return random(160, 331) / 10.0;
+}
+
+// função para gerar um valor de umidade
+float getHumidity()
+{
+  // gerando um valor aleatório até 1000 e dividindo por 10 para transformar o inteiro em um float e
+  // indicar a porcentagem da umidade com até uma casa decimal de precisão
+  return random(1000) / 10.0;
+}
+
+// função para gerar um valor da luminosidade
+float getLuminosity()
+{
+  // gerando um valor aleatório até 1024 (exclusivo) para representar o valor medido pelo sensor LDR
+  return random(1024);
 }
